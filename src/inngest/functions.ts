f4176601f629572c3042d06b04282901853c74bf
Nandugolanukonda <golanukondanandu@gmail.com>
2025-07-03 17:47:22 +0530

    feat: integrate Inngest agent for text summarization
    - Added @inngest/agent-kit dependency to package.json.
    - Updated page.tsx to include an input field for user text and modified the mutation to send the input value.
    - Enhanced functions.ts to create an Inngest agent that summarizes text using the Gemini model.
    - Refactored app router to accept 'value' instead of 'text' for the mutation input.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,20 @@
+import { createAgent, gemini } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "summerizer",
+      system: "You are an expert summerizer. You summarize in two words",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Summerize the following text: ${event.data.value}`
+    );
+    return { output };
   }
 );
