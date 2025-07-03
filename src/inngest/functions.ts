6ea76c1bb0919ed1a2662b8f1e25d34612f54980
Nandugolanukonda <golanukondanandu@gmail.com>
2025-07-03 19:07:22 +0530

    feat: add E2B sandbox template and integrate code interpreter functionality

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,3 +1,5 @@
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "@/inngest/utils";
 import { createAgent, gemini } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
@@ -5,16 +7,27 @@ import { inngest } from "./client";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-nandu");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "summerizer",
-      system: "You are an expert summerizer. You summarize in two words",
+      system:
+        "You are an expert nextjs developer. You write,readable and maitainable code.You write simple Next.js & React snippets",
       model: gemini({ model: "gemini-2.0-flash" }),
     });
 
     const { output } = await codeAgent.run(
       `Summerize the following text: ${event.data.value}`
     );
-    return { output };
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
   }
 );
