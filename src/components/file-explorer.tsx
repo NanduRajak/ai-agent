8eadb870e4359da76bbe3de431dd23d3426e3b27
Nandugolanukonda <golanukondanandu@gmail.com>
2025-07-07 21:43:14 +0530

    code view

---
+++ src/components/file-explorer.tsx
@@ -0,0 +1,166 @@
+import { CopyCheckIcon, CopyIcon } from "lucide-react";
+import { useState, useMemo, useCallback, Fragment } from "react";
+
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { CodeView } from "@/components/code-view";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbEllipsis,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { _toLowerCase } from "zod/v4/core";
+import { convertFilesToTreeItems } from "@/lib/utils";
+import { TreeView } from "./tree-view";
+
+type FileCollection = { [path: string]: string };
+
+function getLanguageFromExtension(filename: string): string {
+  const extention = filename.split(".").pop()?.toLowerCase();
+  return extention || "text";
+}
+
+interface FileBreadCrumbProps {
+  filePath: string;
+}
+
+const FileBreadCrumb = ({ filePath }: FileBreadCrumbProps) => {
+  const pathSegments = filePath.split("/");
+  const maxSegments = 4;
+  const renderBreadcrumItems = () => {
+    if (pathSegments.length <= maxSegments) {
+      // Show all segment if 4 or less
+      return pathSegments.map((segment, index) => {
+        const isList = index === pathSegments.length - 1;
+
+        return (
+          <Fragment key={index}>
+            <BreadcrumbItem>
+              {isList ? (
+                <BreadcrumbPage className="font-medium">
+                  {segment}
+                </BreadcrumbPage>
+              ) : (
+                <span className="text-muted-foreground">{segment}</span>
+              )}
+            </BreadcrumbItem>
+            {!isList && <BreadcrumbSeparator />}
+          </Fragment>
+        );
+      });
+    } else {
+      const firstSegment = pathSegments[0];
+      const lastSegment = pathSegments[pathSegments.length - 1];
+      return (
+        <>
+          <BreadcrumbItem>
+            <span className="text-muted-foreground">{firstSegment}</span>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbEllipsis />
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage className="text-medium">
+                {lastSegment}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbItem>
+        </>
+      );
+    }
+  };
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>{renderBreadcrumItems()}</BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+interface FileExplorerProps {
+  files: FileCollection;
+}
+
+export const FileExplorer = ({ files }: FileExplorerProps) => {
+  const [copied, setCopied] = useState(false);
+  const [selectdFile, setSelectedFile] = useState<string | null>(() => {
+    const fileKeys = Object.keys(files);
+    return fileKeys.length > 0 ? fileKeys[0] : null;
+  });
+
+  const treeData = useMemo(() => {
+    return convertFilesToTreeItems(files);
+  }, [files]);
+
+  const handleFileSelect = useCallback(
+    (filePath: string) => {
+      if (files[filePath]) {
+        setSelectedFile(filePath);
+      }
+    },
+    [files]
+  );
+
+  const handleCopy = useCallback(() => {
+    if (selectdFile) {
+      navigator.clipboard.writeText(files[selectdFile]);
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    }
+  }, [selectdFile, files]);
+
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel defaultSize={30} minSize={30} className="bg-sidebar">
+        <TreeView
+          data={treeData}
+          value={selectdFile}
+          onSelect={handleFileSelect}
+        />
+      </ResizablePanel>
+      <ResizableHandle className="hover:bg-primary transition-colors" />
+      <ResizablePanel defaultSize={70} minSize={50}>
+        {selectdFile && files[selectdFile] ? (
+          <div className="h-full w-full flex flex-col">
+            <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
+              <FileBreadCrumb filePath={selectdFile} />
+              <Hint text="Copy to clipboard" side="bottom">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="ml-auto"
+                  onClick={handleCopy}
+                  disabled={copied}
+                >
+                  {copied ? <CopyCheckIcon /> : <CopyIcon />}
+                </Button>
+              </Hint>
+            </div>
+            <div className="flex-1 overflow-auto">
+              <CodeView
+                code={files[selectdFile]}
+                lang={getLanguageFromExtension(selectdFile)}
+              />
+            </div>
+            <p>TODO: Code view</p>
+          </div>
+        ) : (
+          <div className="flex h-full items-center justify-center text-muted-foreground">
+            Select a file to view it&apos;s content
+          </div>
+        )}
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
