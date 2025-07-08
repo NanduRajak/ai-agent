1095feba9ecf24893fed55fbca60ed8e405f9777
Nandugolanukonda <golanukondanandu@gmail.com>
2025-07-09 01:15:40 +0530

    04:Authentication

--- src/module/projects/ui/views/project-view.tsx
+++ src/module/projects/ui/views/project-view.tsx
@@ -15,7 +15,7 @@ import { EyeIcon, CodeIcon, CrownIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { FileExplorer } from "@/components/file-explorer";
-import path from "path";
+import { UserControl } from "@/components/user-control";
 
 interface Props {
   projectId: string;
@@ -69,6 +69,7 @@ export const ProjectView = ({ projectId }: Props) => {
                     Upgrade
                   </Link>
                 </Button>
+                <UserControl />
               </div>
             </div>
             <TabsContent value="preview">
